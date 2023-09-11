855fb0c4079cacb23f206edc28b3b5da3c607f6d
Diego <122388907+diego-ar@users.noreply.github.com>
2023-09-11 20:07:37 +0200

    Responsive updated

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* toggle icon navbar*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+/* scroll sections avtive link*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = ()=> {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHright;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset &&top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*' + id + ']').classlist.add('Active');
+            });
+        };
+    });
+    /* Sticky navbar*/
+    let header = document.querySelector('header');
+    header.classlist.toggle('sticky', window.scrollY > 100);
+};
