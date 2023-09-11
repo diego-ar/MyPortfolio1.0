fe3b350ff439c1c289e8bb7a254e7fb3b7141f45
Diego <122388907+diego-ar@users.noreply.github.com>
2023-09-11 22:22:34 +0200

    Update of Responsime mode and scripts

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
-    /* scroll sections avtive link*/
+    /* scroll sections active link*/
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
@@ -29,25 +28,22 @@ window.onscroll = ()=> {
     /* Sticky navbar */
     let header = document.querySelector('header');
 
-    header.classlist.toggle('sticky', window.scrollY > 100);
-
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     /* remove toggle icon and navbar when click navbar link (scroll) */
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+    /* Scroll reveal */
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading',  { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .project-box, .contact form',  { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img',  { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
